bbf1e65269b236cfe03b55ccc8d522230fbb6169
Victoria A <victoria.arriola@northwestern.edu>
2021-06-08 22:15:22 -0500

    changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputDate = d3.select("#datetime");
+
+// var data = [{
+//     datetime: "1/1/2010",
+//     city: "benton",
+//     state: "ar",
+//     country: "us",
+//     shape: "circle",
+//     durationMinutes: "5 mins.",
+//     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+//   }
+
+var columns = ["date/time", "city", "state", "country", "shape", "comment"]
+
+function 
+
+ul.html("");
