e3311d4c897e125ef32533800de39ada21dc946c
Victoria A <victoria.arriola@northwestern.edu>
2021-06-09 02:13:15 -0500

    finished?

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,10 +5,9 @@ var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputDate = d3.select("#datetime");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes","comment"]
-tbody.html("");
+
 function addData(dataInput) {
     
-
     dataInput.forEach(sighting => {
         var row = tbody.append("tr");
         columns.forEach(column => row.append("td").text(sighting[column])
@@ -24,13 +23,14 @@ button.on("click", () => {
     var inputValue = inputDate.property("value");
     var filterDate = tableData.filter(d => d.datetime === inputValue);
     
-    
+    tbody.html("");
+
     var userInput = {
         filterDate
     }
     
     if (userInput.filterDate.length !== 0) {
-        buildTable(filterDate);
+        addData(filterDate);
     }
 
         else {
