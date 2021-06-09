37b013a3c6d055df92aaf9a3445ce8b34a1ec98d
Victoria A <victoria.arriola@northwestern.edu>
2021-06-09 01:52:52 -0500

    finish?

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,15 +6,13 @@ var button = d3.select("#filter-btn");
 var inputDate = d3.select("#datetime");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes","comment"]
 
-
-function buildTable(data) {
-        data.forEach(sighting => {
+function addData(dataInput) {
+    dataInput.forEach(sighting => {
         var row = tbody.append("tr");
-        columns.forEach(column => row.append("tr").text(sighting[column])
+        columns.forEach(column => row.append("td").text(sighting[column])
         );
     });
 };
-
 
 addData(tableData);
 
@@ -26,21 +24,17 @@ button.on("click", () => {
     
     tbody.html("");
 
-
-
-
-button.on("click", () => {
-    d3.event.preventDefault();
-
-    var inputValue = inputDate.property("value");
-    var filterDate = tableData.filter(person => person.datetime === inputValue);
+    var userInput = {
+        filterDate
+    }
     
-    tbody.html("");
-
-    if(! == 0) {
-
+    if (userInput.filterDate.length !== 0) {
+        buildTable(filterDate);
     }
 
+        else {
+            tbody.append("tr").append("td").text("Sorry there aren't any results for this date.");
+        }
 
 });
     
